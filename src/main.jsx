caa9fa635739cc5f5de1000bf83f6b4342ad22d8
Wilson-Edem <edsonedem2121@gmail.com>
2026-08-23 09:44:30 +0100

    Remove service worker registration logic
    Removed service worker registration code from main.jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -21,12 +21,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </ThemeProvider>
   </React.StrictMode>
 );
-// Register service worker
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then((reg) => console.log("SW registered:", reg))
-      .catch((err) => console.log("SW registration failed:", err));
-  });
-}
